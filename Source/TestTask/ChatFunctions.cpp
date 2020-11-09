2547daad53629d81f3ee1dcceaefeb8e6b9ba4e1
Sajoniks <avesom33@mail.ru>
2020-11-09 20:21:34 +0300

    Added profanity filter

---
+++ Source/TestTask/ChatFunctions.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChatFunctions.h"
+
+//Array of bad words
+//Can be updated from server, if needed
+static TArray<FString> BadWords = {
+	"gay"
+	"shit"
+	"suck"
+	"dick"
+	"asshole"
+};
+
+/**
+ * Simple profanity filter
+ * Works only on words from the array above
+ * Does not work with plurals
+ * Is not localized
+ * @param InText Text to check
+ * @out Sanitized text
+ */
+FText UChatFunctions::BasicProfanityFilter(const FText& InText)
+{
+	//Beginning of the pattern
+	FString Pattern = "(?i)\b(";
+	
+	for (auto&& w : BadWords)
+	{
+		Pattern.Append(w).Append("|");
+	}
+	
+	//Remove unnecessary '|'
+	Pattern = Pattern.LeftChop(1);
+	Pattern.Append(")\b");
+
+	FRegexPattern RegexPattern(Pattern);
+	FRegexMatcher Matcher(RegexPattern, InText.ToString());
+
+	return FText::FromString("");
+}
---
+++ Source/TestTask/ChatFunctions.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ChatFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTTASK_API UChatFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category="Chat")
+	static FText BasicProfanityFilter(const FText& InText);
+	
+};
