f97b47d89bc2293d0f18557f6c3fa0da397500cf
Sajoniks <avesom33@mail.ru>
2020-11-09 18:00:53 +0300

    Added files
    Font face, style data table and decorator for hyperlinks

---
+++ Source/TestTask/HyperLinkRichTextDecorator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/RichTextBlockDecorator.h"
+#include "HyperLinkRichTextDecorator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTTASK_API UHyperLinkRichTextDecorator : public URichTextBlockDecorator
+{
+	GENERATED_BODY()
+	
+};
