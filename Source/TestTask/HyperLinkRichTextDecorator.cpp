e94940d8c8270f4d783b83482832b8b787063779
Sajoniks <avesom33@mail.ru>
2020-11-09 19:36:02 +0300

    Made hyperlink decorator

--- Source/TestTask/HyperLinkRichTextDecorator.cpp
+++ Source/TestTask/HyperLinkRichTextDecorator.cpp
@@ -3,3 +3,50 @@
 
 #include "HyperLinkRichTextDecorator.h"
 
+#include "Framework/Text/SlateHyperlinkRun.h"
+#include "Widgets/Input/SRichTextHyperlink.h"
+
+FRichInlineHyperLinkDecorator::FRichInlineHyperLinkDecorator(URichTextBlock* InOwner,
+                                                             UHyperLinkRichTextDecorator* Decorator):
+FRichTextDecorator(InOwner)
+{
+	Style = Decorator->Style;
+	Delegate.BindLambda([&]()
+	{
+		Decorator->OnClick(StringLink);
+	});
+}
+
+bool FRichInlineHyperLinkDecorator::Supports(const FTextRunParseResults& RunInfo, const FString& Text) const
+{
+	if (RunInfo.Name.Equals("link"))
+	{
+		const auto& Range = RunInfo.ContentRange;
+		
+		StringLink = Text.Mid(Range.BeginIndex, Range.EndIndex - Range.BeginIndex);
+		return true;
+	}
+
+	return false;
+}
+
+TSharedPtr<SWidget> FRichInlineHyperLinkDecorator::CreateDecoratorWidget(const FTextRunInfo& RunInfo,
+	const FTextBlockStyle& DefaultTextStyle) const
+{	
+	TSharedPtr<FSlateHyperlinkRun::FWidgetViewModel> Model = MakeShareable(new FSlateHyperlinkRun::FWidgetViewModel);
+	TSharedPtr<SRichTextHyperlink> Link = SNew(SRichTextHyperlink, Model.ToSharedRef())
+		.Text(FText::FromString(StringLink))
+		.Style(&Style)
+		.OnNavigate(Delegate);
+
+	return Link;
+	
+}
+
+TSharedPtr<ITextDecorator> UHyperLinkRichTextDecorator::CreateDecorator(URichTextBlock* InOwner)
+{
+	return MakeShareable(new FRichInlineHyperLinkDecorator{
+		InOwner,
+		this
+	});
+}
--- Source/TestTask/HyperLinkRichTextDecorator.h
+++ Source/TestTask/HyperLinkRichTextDecorator.h
@@ -6,12 +6,45 @@
 #include "Components/RichTextBlockDecorator.h"
 #include "HyperLinkRichTextDecorator.generated.h"
 
-/**
- * 
- */
+class FRichInlineHyperLinkDecorator : public FRichTextDecorator
+{
+	
+public:
+	FRichInlineHyperLinkDecorator(URichTextBlock* InOwner, class UHyperLinkRichTextDecorator* Decorator);
+
+	//This decorator handles "link" tag
+	bool Supports(const FTextRunParseResults& RunInfo, const FString& Text) const override;
+
+	TSharedPtr<SWidget> CreateDecoratorWidget(const FTextRunInfo& RunInfo, const FTextBlockStyle& DefaultTextStyle) const override;
+
+	//Using UE4 link style
+	FHyperlinkStyle Style;
+
+	//Delegate which called on link click
+	FSimpleDelegate Delegate;
+
+	//Link
+	mutable FString StringLink;
+};
+
+
 UCLASS()
 class TESTTASK_API UHyperLinkRichTextDecorator : public URichTextBlockDecorator
 {
 	GENERATED_BODY()
+
+public:
+
+	UHyperLinkRichTextDecorator(const FObjectInitializer& ObjectInitializer):
+		URichTextBlockDecorator(ObjectInitializer) {}
 	
+	TSharedPtr<ITextDecorator> CreateDecorator(URichTextBlock* InOwner) override;
+
+
+	UPROPERTY(EditAnywhere, Category = "Appearence")
+		FHyperlinkStyle Style;
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Action")
+		void OnClick(const FString& Link);
+
 };
